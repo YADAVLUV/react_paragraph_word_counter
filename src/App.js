93bb051bd43b078a31c61489bc4f558ec212b9e1
love <yadavlove909@gmail.com>
2023-10-10 13:35:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // You can style it as needed
 
 function App() {
+  const [text, setText] = useState('');
+  
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const wordCount = text.trim().split(/\s+/).length;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Responsive Word Counter</h1>
+      <div className="textarea-container">
+        <textarea
+          rows="5"
+          placeholder="Type your text here..."
+          value={text}
+          onChange={handleChange}
+        ></textarea>
+        <div className="word-count">Word Count: {wordCount}</div>
+      </div>
     </div>
   );
 }
